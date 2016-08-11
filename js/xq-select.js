75fc444e31fb15718a5ba4b2bf59b4adbda15b57
amjones <amj@networkidea.com>
2016-08-10 21:06:19 -0500

    Addition of SASS support.  Also changes compiled CSS to utilize SCSS source, rather than LESS.  LESS support will be maintained through version 2.0.

--- js/xq-select.js
+++ js/xq-select.js
@@ -3,7 +3,7 @@ $( document ).ready( function () {
 } );
 
 /**
- * xqSelect v1.1.5 (https://github.com/exactquery/xq-select)
+ * xqSelect v (https://github.com/exactquery/xq-select)
  * @author  Aaron M Jones [aaron@jonesiscoding.com]
  * @licence MIT (https://github.com/exactquery/xq-select/blob/master/LICENSE)
  */
